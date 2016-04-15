6dd4fde9b490b41b4c2fcfba1324ae77d97b57ec
Connor Snee <snee0614@gmail.com>
2016-04-15 10:35:37 -0400

    fixed imports

--- src/test/Pancheros.java
+++ src/test/Pancheros.java
@@ -3,9 +3,7 @@ package test;
 import javax.swing.*;
 
 import java.util.Random;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class Pancheros {
 	
